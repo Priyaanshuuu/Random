ced756f8b84bde3d8d10272dcdf0146d9ec4beee
Priyanshu <priyanshusinha636@gmail.com>
2026-07-22 20:16:43 +0530

    Prompt_Schema

---
+++ Prompt_opt/frontend/prisma/migrations/20260722144554_add_prompt_version/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `PromptVersion` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PromptVersion" DROP COLUMN "description";
